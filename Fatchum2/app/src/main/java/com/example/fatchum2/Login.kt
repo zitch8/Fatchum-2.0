547acafce5a854256aea2921d9d5e39eade9cba6
zitch8 <darendasallas27@gmail.com>
2024-05-11 00:38:47 +0800

    Update Login.kt

--- Fatchum2/app/src/main/java/com/example/fatchum2/Login.kt
+++ Fatchum2/app/src/main/java/com/example/fatchum2/Login.kt
@@ -2,11 +2,22 @@ package com.example.fatchum2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+    // For Firebase Account Authentication
+    private lateinit var auth: FirebaseAuth
+
+    // For Remember me Shared Preferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Firebase Auth
+        auth = Firebase.auth
+
+        //Save Email and Password
     }
 }
