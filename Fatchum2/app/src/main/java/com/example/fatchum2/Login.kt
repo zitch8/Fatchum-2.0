875fb04dd05d06c448a4e9d885ec07d85f1dbae5
zitch8 <darendasallas27@gmail.com>
2024-05-11 22:02:41 +0800

    added fragments

--- Fatchum2/app/src/main/java/com/example/fatchum2/Login.kt
+++ Fatchum2/app/src/main/java/com/example/fatchum2/Login.kt
@@ -1,14 +1,18 @@
 package com.example.fatchum2
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -20,42 +24,67 @@ class Login : AppCompatActivity() {
     // For Remember me Shared Preferences
     private lateinit var emailText: EditText
     private lateinit var pwdText: EditText
+    private lateinit var info: TextView
+
     private lateinit var sf: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+        val register = findViewById<TextView>(R.id.tvRegister)
+        
         // Initialize Firebase Auth
         auth = Firebase.auth
 
         //Save Email and Password
         emailText = findViewById(R.id.editEmail)
         pwdText = findViewById(R.id.editPassword)
+        info = findViewById<TextView>(R.id.tvInfoLogin)
         sf = getSharedPreferences("rememberMe", MODE_PRIVATE)
         editor = sf.edit()
+
+        // Register Click Listener
+        register.setOnClickListener {
+            val openRegister = Intent(this@Login, Register::class.java)
+            startActivity(openRegister)
+            finish()
+        }
+        //Login Button Click Listener
+        loginButton.setOnClickListener { login() }
+
+        // Remove Info Text When Edit Text is Clicked
+        val onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                info.text = ""
+            }
+        }
+        emailText.onFocusChangeListener = onFocusChangeListener
+        pwdText.onFocusChangeListener = onFocusChangeListener
+
     }
 
     private fun login(){
-        val email = findViewById<EditText>(R.id.editEmail)
-        val password = findViewById<EditText>(R.id.editPassword)
-        val info = findViewById<TextView>(R.id.tvInfoLogin)
-
         // Check Null Input
-        if (email.text.isEmpty() || password.text.isEmpty()){
+        if (emailText.text.isEmpty() || pwdText.text.isEmpty()){
             Toast.makeText(this, "Email or Password must not be empty.", Toast.LENGTH_SHORT).show()
             info.text = "Email and password must not be empty."
             return
         } else{
-            val emailCheck = email.text.toString()
-            val passwordCheck = password.text.toString()
+            val emailCheck = emailText.text.toString()
+            val passwordCheck = pwdText.text.toString()
 
-            // If Remember Me is on
 
             auth.signInWithEmailAndPassword(emailCheck, passwordCheck)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful){
                         Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+                        val openMain = Intent(this@Login, Main::class.java)
+                        startActivity(openMain)
+                        finish()
                     } else {
                         Log.w(TAG, "signInWithEmail:failure", task.exception)
                         info.text = "Invalid email or password."
---
+++ Fatchum2/app/src/main/java/com/example/fatchum2/Main.kt
@@ -0,0 +1,38 @@
+package com.example.fatchum2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class Main : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initial fragment
+        replaceFragment(Home())
+
+        // Change fragment when bottom nav icon is clicked
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.home -> replaceFragment(Home())
+                    R.id.favorite -> replaceFragment(Favorite())
+                    R.id.profile -> replaceFragment(Profile())
+                    else -> {}
+                }
+                true
+            }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameMain, fragment)
+        fragmentTransaction.commit()
+    }
+}
